a3565c19c10c4f88074876b32f03e18a404fc944
Tanous1991 <tounsi.oussama.to@gmail.com>
2018-01-11 11:35:04 +0100

    11-01-2018-11:35

--- src/app/Detail/Detail.component.ts
+++ src/app/Detail/Detail.component.ts
@@ -3,6 +3,7 @@ import { PaperService } from './../app.service.ts.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { TruncateModule } from 'ng2-truncate';
 
 @Component({
   selector: 'app-Detail',
@@ -17,7 +18,11 @@ export class DetailComponent implements OnInit {
   tab1: boolean = false;
   tab2: boolean = false;
   id: String;
-
+  show1 : String = "Show more";
+  show2 : String = "Show more";
+  limit1 : Number = 300;
+  limit2 : Number = 3 ;
+  
   constructor(private route: ActivatedRoute,
     private PaperService: PaperService) { }
 
@@ -52,5 +57,27 @@ export class DetailComponent implements OnInit {
     console.log("outCitationstab = " + outCitationstab);
     await this.PaperService.getPaper(outCitationstab).then(resData => this.outCitations = resData);
     console.log("outCitations = " + this.outCitations.length);
+  }
+
+  showmore(x : String){
+    if(x == "1"){
+      if(this.limit1 == 300){
+        this.limit1 = 300000000000000;
+        this.show1 = "show less";
+      }else{
+        this.limit1 = 300;
+        this.show1 = "show more";
+      }
+    }
+    if(x == "2"){
+      if(this.limit2 == 3){
+        this.limit2 = 100;
+        this.show2 = "Show less";
+      }else{
+        this.limit2 = 3;
+        this.show2 = "Show more";
+      }
+    }
+
   }
 }
